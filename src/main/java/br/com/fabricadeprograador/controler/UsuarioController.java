2980a229767bbcfb9eb1c9a9c15d2bcf676f74a2
Édipo <edipogb@gmail.com>
2015-07-09 22:59:47 -0400

    Atualização

--- src/main/java/br/com/fabricadeprograador/controler/UsuarioController.java
+++ src/main/java/br/com/fabricadeprograador/controler/UsuarioController.java
@@ -3,10 +3,13 @@ package br.com.fabricadeprograador.controler;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet("/usucontroller")
 public class UsuarioController extends HttpServlet {
 	/**
 	 * 
